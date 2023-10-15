d26aa287412b1932206dfec5b8c1e99649ed8f0d
Rayhan Illyas <rayhan205732@gmail.com>
2023-10-15 23:24:58 +0700

    feat: init layout for community, plans, and settings

---
+++ app/src/main/java/com/example/wanderlusty/feature_community/presentation/CommunityActivity.kt
@@ -0,0 +1,12 @@
+package com.example.wanderlusty.feature_community.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.wanderlusty.R
+
+class CommunityActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_community)
+    }
+}
---
+++ app/src/main/java/com/example/wanderlusty/feature_plans/presentation/PlansActivity.kt
@@ -0,0 +1,12 @@
+package com.example.wanderlusty.feature_plans.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.wanderlusty.R
+
+class PlansActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_plans)
+    }
+}
